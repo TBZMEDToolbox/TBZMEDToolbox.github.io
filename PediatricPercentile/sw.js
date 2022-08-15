5db75b6e6e36bbdfbaa52082531ddaf27c92f6d5
RasaKh <109285655+TBZMEDToolbox@users.noreply.github.com>
2022-08-15 19:29:12 +0430

    Readded cache control

--- PediatricPercentile/sw.js
+++ PediatricPercentile/sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/PediatricPercentile';
 var APP_PREFIX = 'tbzrkhpp_';
-var VERSION = 'version 0014';
+var VERSION = 'version 0015';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
