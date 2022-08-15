e4ced73e6fb05ffe3680207e0dfa2250adfd6ba5
RasaKh <109285655+TBZMEDToolbox@users.noreply.github.com>
2022-08-15 19:25:55 +0430

    Version Update

--- PediatricPercentile/sw.js
+++ PediatricPercentile/sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/PediatricPercentile';
 var APP_PREFIX = 'tbzrkhpp_';
-var VERSION = 'version 0013';
+var VERSION = 'version 0014';
 var URLS = [    
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
